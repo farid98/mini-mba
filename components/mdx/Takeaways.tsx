79c285243a83cde10ff993528173bdc105b4bbb8
Farid <farid98@gmail.com>
2026-06-28 11:14:10 +0500

    in this chapter

--- components/mdx/Takeaways.tsx
+++ components/mdx/Takeaways.tsx
@@ -6,7 +6,7 @@ export default function Takeaways({ children }: TakeawaysProps) {
   return (
     <aside className="bg-teal-light border-l-[3px] border-teal rounded-r-[6px] py-4 pl-6 pr-5 my-8">
       <div className="font-bold text-[0.6875rem] uppercase tracking-[0.1em] text-teal mb-[0.625rem]">
-        Key Takeaways
+        In this chapter
       </div>
       <div className="takeaways-body">{children}</div>
     </aside>
